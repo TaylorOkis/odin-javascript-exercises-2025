1e06f2ffd5ddae4c050f34043367bf9b7c909325
Tammy Taylor <72969270+TaylorOkis@users.noreply.github.com>
2025-05-22 11:28:02 +0100

    Add findTheOldest exercise solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    people.sort((a, b) => {
+        let aAge = (a.yearOfDeath ? a.yearOfDeath : new Date(Date.now()).getFullYear()) - a.yearOfBirth;
+        let bAge = (b.yearOfDeath ? b.yearOfDeath : new Date(Date.now()).getFullYear()) - b.yearOfBirth;
+        return bAge - aAge;
+    })
+    return people[0];
 };
+
+function compareYears(a, b) {
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
