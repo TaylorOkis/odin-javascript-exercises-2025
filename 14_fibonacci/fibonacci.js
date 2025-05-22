722e3022ee1779706f206233f2f7f24685cb4ddc
Tammy Taylor <72969270+TaylorOkis@users.noreply.github.com>
2025-05-22 10:45:26 +0100

    Add fibonacci exercise solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = parseInt(num);
+    if (num < 0) return "OOPS";
 
+    let initialNumber = 1;
+    let fibonacciNumber = 0;
+    let tempNumber;
+    for (let i = 0; i < num; i++) {
+        tempNumber = fibonacciNumber;
+        fibonacciNumber += initialNumber;
+        initialNumber = tempNumber;
+    }
+
+    return fibonacciNumber;
 };
 
 // Do not edit below this line
