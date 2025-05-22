471bae014d69644e4df0e85edbf87ef0f0c9eab1
Tammy Taylor <72969270+TaylorOkis@users.noreply.github.com>
2025-05-22 12:23:59 +0100

    Add palindromes exercise solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const filteredCharacters = word
+        .toLowerCase()
+        .split("")
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+    const reversedCharacters = filteredCharacters.split('').reverse().join('');
+    return reversedCharacters === filteredCharacters;
 };
 
 // Do not edit below this line
